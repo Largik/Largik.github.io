e2a7baa9af7f3f6a3eb95f3d60103803ff2d36af
Largik <57515916+Largik@users.noreply.github.com>
2021-04-20 22:44:55 +0400

    lab5 is ready

--- addNews/addNews.js
+++ addNews/addNews.js
@@ -1,32 +1,62 @@
 console.log("Скрипт загружен")
 let formData = new FormData();
 
-var buttonsubmit = document.forms[0].elements[1];
-buttonsubmit.addEventListener( "click" , buttonPressed);
+var buttonsubmit = document.forms[1].buttonSubmit;
+buttonsubmit.addEventListener("click", buttonPressed);
 
 function buttonPressed(evt) {
 	console.log("Кнопка нажата загружен");
 	evt.preventDefault();
-	
-	if(document.forms[0].elements[0].value == 'user'){
-	Swal.fire(
+
+	if (document.forms[1].FormInputHeaderNews.value == '') {
+		Swal.fire(
 			'Ошибка!',
 			'Вы не ввели наименование новости!',
 			'error'
 		)
 		console.log("Вы не ввели наименование новости!");
+		return
 	}
-	else{
-		formData.append('newsName', document.forms[0].elements[0].value);
+	if (document.forms[1].FormInputNews.value == '') {
+		Swal.fire(
+			'Ошибка!',
+			'Вы не написали новость!',
+			'error'
+		)
+		console.log("Вы не написали новость!");
+		return
+	}
+	if (document.forms[1].FormInputAuthor.value == '' && document.forms[1].invalidCheck.checked == false) {
+		Swal.fire(
+			'Ошибка!',
+			'Вы не ввели автора! Или не поставили галочку в поле Анонимно.',
+			'error'
+		)
+		console.log("Вы не ввели автора! Или не поставили галочку в поле Анонимно.");
+		return
+	}
+	formData.append('newsName', document.forms[1].FormInputHeaderNews.value);
+	formData.append('newsAuthor', document.forms[1].FormInputAuthor.value);
+	if (document.forms[1].invalidCheck.checked == false) {
 		Swal.fire({
 			position: "center",
 			icon: "success",
-			title: "Наименование новости " + formData.get("newsName"),
+			title: "Наименование новости " + formData.get("newsName") + " под аворством " + formData.get("newsAuthor"),
 			showConfirmButton: false,
 			timer: 1500
 		}
 		)
-		setTimeout(function() {document.location.href = "";},1500);
-		console.log("Новость добавлена в предложенные");
+	} else {
+		Swal.fire({
+			position: "center",
+			icon: "success",
+			title: "Наименование новости " + formData.get("newsName") + " анонимно",
+			showConfirmButton: false,
+			timer: 1500
+		}
+		)
 	}
+	setTimeout(function () { document.location.href = ""; }, 1500);
+	console.log("Новость добавлена в предложенные");
+
 }
